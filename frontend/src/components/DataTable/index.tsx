4dd623d8aedc4e688f7ab127a44b600f11540ce1
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2021-09-06 21:59:22 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,56 @@
+function DataTable() {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Guilherme Shibuya</td>
+                        <td>23</td>
+                        <td>22</td>
+                        <td>10587.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Guilherme Shibuya</td>
+                        <td>23</td>
+                        <td>22</td>
+                        <td>10587.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Guilherme Shibuya</td>
+                        <td>23</td>
+                        <td>22</td>
+                        <td>10587.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Guilherme Shibuya</td>
+                        <td>23</td>
+                        <td>22</td>
+                        <td>10587.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Guilherme Shibuya</td>
+                        <td>23</td>
+                        <td>22</td>
+                        <td>10587.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
