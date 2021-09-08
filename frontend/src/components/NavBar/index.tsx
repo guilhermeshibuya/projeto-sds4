039f330b8403e385adedc498225965787f069bdf
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2021-09-08 16:00:17 -0300

    Security config

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -5,7 +5,7 @@ function NavBar() {
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={ImgDsDark} alt="DevSuperior" width="120"></img>
+                    <h2 className="text-primary">SHIBUYA</h2>
                 </nav>
             </div>
         </div>
